084d61d482784f16cba8371bd513d760111fcd84
Jason Macbook Air <jason@jasonmcdermott.net>
2014-10-02 00:07:47 +1000

    New copy just for XC6

---
+++ CodeCamp For Xcode 6.xctemplate/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AudioToolbox/AudioToolbox.h>
+
+#import "AppDelegate.h"
+#import "CodeCamp.h"
+
+@interface ViewController : UIViewController
+
+@end
